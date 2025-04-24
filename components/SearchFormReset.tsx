39d5777f46c3d322db34b35d51997b59500d294f
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-04-24 20:41:23 +0200

    add search form component

---
+++ components/SearchFormReset.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import Link from "next/link";
+import {X} from "lucide-react";
+import {Button} from "@/components/ui/button";
+
+const SearchFormReset = () => {
+    const reset = () => {
+        const form = document.querySelector('.search-form') as HTMLFormElement;
+        if (form) form.reset();
+    }
+
+    return (<Button type="reset" onClick={reset} className="search-btn text-white">
+        <Link href="/"><X className="size-5" /></Link>
+    </Button>)
+}
+export default SearchFormReset
