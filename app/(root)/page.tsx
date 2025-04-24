f246ce61e7dedabdbecb3367b324bc20a20ac37e
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-04-24 19:49:30 +0200

    add theme styles

---
+++ app/(root)/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (<>
+        <h1 className="heading">
+          Pitch Your Startup, <br/>
+          Connect With Entrepreneurs
+        </h1>
+      </>);
+}
