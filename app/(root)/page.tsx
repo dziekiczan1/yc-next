0064079a08b71265ad0cc56fe815cde2969bdee1
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-04-28 19:32:53 +0200

    adjust startup query to to match search results

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,25 @@
 import SearchForm from "@/components/SearchForm";
-import StartupCard, {StartupTypeCard} from "@/components/StartupCard";
-import {STARTUPS_QUERY} from "@/sanity/lib/queries";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { auth } from "@/auth";
 
-export default async function Home({searchParams}: {searchParams: Promise<{query?:string}>}) {
+export default async function Home({
+    searchParams,
+}: {
+    searchParams: Promise<{ query?: string }>;
+}) {
     const query = (await searchParams).query;
-    const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
+    const params = { search: query || null };
+    const session = await auth();
 
-    return (<>
-            <section className="pink_container pattern">
+    const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
+
+    return (
+        <>
+            <section className="pink_container">
                 <h1 className="heading">
-                    Pitch Your Startup, <br/>
+                    Pitch Your Startup, <br />
                     Connect With Entrepreneurs
                 </h1>
 
@@ -19,7 +28,7 @@ export default async function Home({searchParams}: {searchParams: Promise<{query
                     Competitions.
                 </p>
 
-                <SearchForm query={query}/>
+                <SearchForm query={query} />
             </section>
 
             <section className="section_container">
@@ -28,11 +37,17 @@ export default async function Home({searchParams}: {searchParams: Promise<{query
                 </p>
 
                 <ul className="mt-7 card_grid">
-                    {posts?.length > 0 ? (posts.map((post: StartupTypeCard) => (
-                            <StartupCard key={post?._id} post={post}/>))) : (
-                        <p className="no-results">No startups found</p>)}
+                    {posts?.length > 0 ? (
+                        posts.map((post: StartupTypeCard) => (
+                            <StartupCard key={post?._id} post={post} />
+                        ))
+                    ) : (
+                        <p className="no-results">No startups found</p>
+                    )}
                 </ul>
             </section>
-        <SanityLive />
-        </>);
+
+            <SanityLive />
+        </>
+    );
 }
